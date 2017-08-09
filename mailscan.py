3fef46ee5159072afbf19231f53db1874c302e9c
Tobias Kessels <tobias.kessels@certbw.de>
2017-08-09 12:34:09 +0200

    added fields and simple sanatization
    csv output now includes date and from field of message
    if fielddata contains any ';' they will be swapped for ','
    
    TODO:
    Date Field has to be parsed and reformatted to a unified timezone (i.e. UTC)
    Multithreading - either one job per postbox or several workers for mail parsing

--- mailscan.py
+++ mailscan.py
@@ -3,30 +3,40 @@
 import os
 import email
 import hashlib
+import sys
+import re
 
-basepath="/tank/data/cases/2017031310000018_mileng_coe/exchange/Mailbox Database 0825066855/Mailbox Database 0825066855.edb"
+# basepath="/tank/data/cases/2017031310000018_mileng_coe/exchange/Mailbox Database 0825066855/Mailbox Database 0825066855.edb"
 # basepath="/tank/data/cases/2017031310000018_mileng_coe/exchange/Mailbox Database 0825066855/Mailbox Database 0825066855.edb/Director DEU OF-5 Janeke"
-
-# traverse root directory, and list directories as dirs and files as files
+basepath=sys.argv[1]
+basecount=len(basepath.split(os.sep))-1
 for root, dirs, files in os.walk(basepath):
     path = root.split(os.sep)
-    # print((len(path) - 1) * '---', os.path.basename(root))
+    relpath = os.sep.join(root.split(os.sep)[basecount:])
     for file in files:
-        # print(len(path) * '---', file)
         filename=root+os.sep+file
+        relfilename=relpath+os.sep+file
         try:
-            # p = sub.Popen('file '+filename,stdout=sub.PIPE,stderr=sub.PIPE)
-            # output, errors = p.communicate()
-            # print(output)
 
             # file_handle=open(root+os.sep+file)
             msg=email.message_from_file(open(filename,encoding="latin-1"))
             if msg.is_multipart():
-                pass
+                if "From" in msg.keys():
+                    msg_from=msg.get("From").replace(";",",")
+                else:
+                    msg_from="UNKNOWN"
+                if "Date" in msg.keys():
+                    msg_date=msg.get("Date").replace(";",",")
+                else:
+                    msg_date="UNKNOWN"
+
                 for part in msg.walk():
                     if part.get_filename() is not None:
-                        md5=hashlib.md5(part.get_payload(decode=True))
-                        print("%s;%s;%s;%s" % (part.get_filename(),part.get_content_type(),md5.hexdigest(),filename))
+                        att_hash=hashlib.md5(part.get_payload(decode=True)).hexdigest()
+                        att_filename=part.get_filename().replace(";",",")
+                        att_mimetype=part.get_content_type().replace(";",",")
+                        msg_filename=relfilename.replace(";",",")
+                        print("%s;%s;%s;%s;%s;%s" % (att_filename,att_mimetype,att_hash,msg_date,msg_from,msg_filename))
                         # print("%s;%s" % (part.get_filename(),md5.hexdigest()))
             else:
                 pass
