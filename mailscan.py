3571ae3e34c76f5553acc374d1cd75b7b87d2711
Tobias Kessels <tabledevil@gmail.com>
2017-08-17 21:07:02 +0200

    Initial timelining capabilities in mailscan implementen

--- mailscan.py
+++ mailscan.py
@@ -6,7 +6,13 @@ import hashlib
 import sys
 import re
 import subprocess
+from dateutil import parser
+from pytz import timezone
 from getallfromfields import Eml
+
+def get_csv_string(*args):
+    return ";".join([" ".join(str(argument).split()).strip().replace(";",",") for argument in args])
+
 
 
 basepath=sys.argv[1]
@@ -15,36 +21,30 @@ for root, dirs, files in os.walk(basepath):
     path = root.split(os.sep)
     relpath = os.sep.join(root.split(os.sep)[basecount:])
     for file in files:
+        # print("=============================================================================================")
         filename=root+os.sep+file
         relfilename=relpath+os.sep+file
         try:
 
             # file_handle=open(root+os.sep+file)
             msg=email.message_from_file(open(filename,encoding="latin-1"))
-            if msg.is_multipart():
-                if "From" in msg.keys():
-                    msg_from=msg.get("From").replace(";",",")
-                else:
-                    msg_from="UNKNOWN"
-                if "Date" in msg.keys():
-                    msg_date=msg.get("Date").replace(";",",")
-                else:
-                    msg_date="UNKNOWN"
+            for rec in msg.get_all("Received"):
+                record=" ".join(rec.split())
+                stringrest,stringdate=record.split(";")
 
-                for part in msg.walk():
-                    if part.get_filename() is not None:
-                        att_hash=hashlib.md5(part.get_payload(decode=True)).hexdigest()
-                        att_filename=part.get_filename().replace(";",",")
-                        att_mimetype=part.get_content_type().replace(";",",")
-                        msg_filename=relfilename.replace(";",",")
-                        print("%s;%s;%s;%s;%s;%s" % (att_filename,att_mimetype,att_hash,msg_date,msg_from,msg_filename))
-                        # print("%s;%s" % (part.get_filename(),md5.hexdigest()))
-            else:
-                pass
+                dt = parser.parse(stringdate)
+                ts_utc=dt.astimezone(tz=timezone('UTC'))
+                msg_id=msg.get("Message-ID")
+                # print(stringdate)
+                # print(dt)
+                # print("%s  | %s | %s"%(str(ts_utc),msg_id,stringrest))
+                print(get_csv_string(str(ts_utc),msg_id,stringrest))
 
-
-
+            # for key in msg.items():
+            #
+            #     print(key)
 
         except Exception as e:
-            print("-"+filename)
-            print (e)
+            # print("-"+filename)
+            # print (e)
+            continue
