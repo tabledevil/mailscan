2bafabbc3a6099ee5f428f52e03d7ef483f1f29b
TKE <tobias.kessels@certbw.de>
2021-01-14 14:36:59 +0100

    Rework decoding of headers

--- mailheader.py
+++ mailheader.py
@@ -2,6 +2,25 @@
 # -*- coding: utf-8 -*-
 import email
 import sys
+import chardet
+
+def fdecode(string):
+    if isinstance(string, str):
+        text, encoding = email.header.decode_header(string)[0]
+        if encoding is None:
+            return text
+        else:
+            return text.decode(encoding)
+    if isinstance(string, bytes):
+        encodings = ['utf-8-sig', 'utf-16', 'iso-8859-15']
+        detection = chardet.detect(string)
+        if "encoding" in detection and len(detection["encoding"]) > 2:
+            encodings.insert(0,detection["encoding"])
+        for encoding in encodings:
+            try:
+                return string.decode(encoding)
+            except UnicodeDecodeError:
+                pass
 
 with open(sys.argv[1],'r',encoding='latin-1') as f:
     msg_data=f.read()
@@ -12,5 +31,6 @@ for (k,v) in msg.items():
         if encoding != None:
             value=string.decode(encoding)
         else:
-            value=string
-        print("{}: {} ({})".format(k,value,type(value)))
+            value=fdecode(string)
+        value=' '.join(value.split())
+        print("{}: {}".format(k,value.strip()))
