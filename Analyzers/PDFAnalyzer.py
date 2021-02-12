43628d95aba6c93307996cf959c4940b0544a383
TKE <tobias.kessels@certbw.de>
2021-02-12 18:16:57 +0100

    Reorganize PDFAnalyzer

--- Analyzers/PDFAnalyzer.py
+++ Analyzers/PDFAnalyzer.py
@@ -6,31 +6,36 @@ class PDFAnalyzer(Analyzer):
     compatible_mime_types = ['application/pdf']
     description = 'PDF Analyser'
 
+    def parse_pdf(self):
+        import io, PyPDF2
+        fileobj = io.BytesIO(self.struct.rawdata)
+        self.pdfobj = PyPDF2.PdfFileReader(fileobj)
+        self.page_count = self.pdfobj.numPages
+        self.reports['pagecount'] = Report(f'{self.pdfobj.numPages}', label="Pages")
+
+        info = self.pdfobj.getDocumentInfo()
+        if info.title:
+            self.reports['title'] = Report(f'{info.title}', label="title")
+        if info.subject:
+            self.reports['subject'] = Report(f'{info.subject}', label="subject")
+        if info.creator:
+            self.reports['creator'] = Report(f'{info.creator}', label="creator")
+        if info.author:
+            self.reports['author'] = Report(f'{info.author}', label="Author")
+        if info.producer:
+            self.reports['producer'] = Report(f'{info.producer}', label="producer")
+
+
     def get_text(self):
-        try:
-            self.page_count = self.pdfobj.numPages
-            txt = ""
+        if hasattr(self,"pdfobj"):
+            text = ""
             for page in range(self.page_count):
                 page_object = self.pdfobj.getPage(page)
-                txt += page_object.extractText()
-            self.text = txt
-            self.reports['pagecount'] = Report(f'{self.pdfobj.numPages}', label="Pages")
-            info = self.pdfobj.getDocumentInfo()
-            if info.title:
-                self.reports['title'] = Report(f'{info.title}', label="title")
-            if info.subject:
-                self.reports['subject'] = Report(f'{info.subject}', label="subject")
-            if info.creator:
-                self.reports['creator'] = Report(f'{info.creator}', label="creator")
-            if info.author:
-                self.reports['author'] = Report(f'{info.author}', label="Author")
-            if info.producer:
-                self.reports['producer'] = Report(f'{info.producer}', label="producer")
-            if len(self.text) > 0:
-                self.childitems.append(self.generate_struct(data=self.text.encode(), mime_type="text/plain"))
-
-        except Exception as e:
-            raise AnalysisModuleException(e)
+                text += page_object.extractText()
+            if len(text) > 0:
+                self.childitems.append(self.generate_struct(data=text.encode(), mime_type="text/plain"))
+                import textwrap
+                self.reports['text'] = Report(text, short=textwrap.shorten(text, width=100))
 
     def getAttachments(self):
         catalog = self.pdfobj.trailer["/Root"]
@@ -46,11 +51,9 @@ class PDFAnalyzer(Analyzer):
         self.embedded_files = attachments
 
     def analysis(self):
-        import io, PyPDF2
-        fileobj = io.BytesIO(self.struct.rawdata)
-        self.pdfobj = PyPDF2.PdfFileReader(fileobj)
-        self.text = ""
-        self.childitems = []
+        #self.text = ""
+        #self.childitems = []
+        self.modules['parser'] = self.parse_pdf
         self.modules['pdf2text'] = self.get_text
         # self.modules['embeddedFiles'] = self.getAttachments
         super().analysis()
