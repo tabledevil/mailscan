65ebaf7b06f2c94c47b7774887e1ee71f9eb3fd5
TKE <tobias.kessels@certbw.de>
2021-04-22 17:28:00 +0200

    Implement debug switch

--- matt.py
+++ matt.py
@@ -3,7 +3,7 @@
 import argparse
 import os
 import structure
-
+from Config.config import flags
 
 if __name__ == "__main__":
     path = os.path.join(os.getcwd(),'extract')
@@ -11,9 +11,12 @@ if __name__ == "__main__":
     parser.add_argument('files', nargs='+', help="Mailfiles to analyse")
     parser.add_argument("-x", "--extract", help="Save all parts of the mail to files", action="store_true")
     parser.add_argument("-f", "--filenames", help="Restore original Filenames", action="store_true")
+    parser.add_argument("-d", "--debug", help="Enable Debug output", action="store_true")
     parser.add_argument("-o", "--out-dir", help=f"output dir. [default={path}]", default=path)
     args = parser.parse_args()
     
+    flags.debug=args.debug
+
     for f in args.files:
         fpath=os.path.join(path,f)
         s = structure.Structure(filename=f)
